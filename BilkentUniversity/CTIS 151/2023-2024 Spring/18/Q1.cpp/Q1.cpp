c6e444283bb442106b5f512f03753ff35d1f6083
Selin Bahar Gündoğar <82098424+SelinBaharGundogar@users.noreply.github.com>
2024-05-09 09:43:16 +0300

    Updating file at BilkentUniversity/CTIS 151/2023-2024 Spring/18/Q1.cpp/Q1.cpp

--- BilkentUniversity/CTIS 151/2023-2024 Spring/18/Q1.cpp/Q1.cpp
+++ BilkentUniversity/CTIS 151/2023-2024 Spring/18/Q1.cpp/Q1.cpp
@@ -1,42 +1,68 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#define SIZE 4
+#define MAX 99
 
-int summation(int num1, int num2) {
-	int total=0;
-	for (int i = 1; i <= num1; i++) {
-		total += num2;
+
+int readFromFile(double grades[][4], int id[], FILE *file)
+{
+	int i = 0;
+
+
+	while (fscanf(file, "%d %d %d %d %d", &id[i], &grades[i][1], &grades[i][2], &grades[i][3], &grades[i][4]) != EOF)
+	{
+		i++;
+	};
+	return(i);
+}
+
+void display(double grades[][4], int id[], int numOfStudents, char ch[], double avg[])
+{
+	int r, c;
+	
+	printf("ID \t R \t L \t S \t W \t ELIGIBLE \t OVERALL\n --------------------------------------\n");
+	for (r = 0; r < numOfStudents; r++)
+		for (c = 0; c < 5; c++)
+			printf("%d \t %.2f \t %.2f \t %.2f \t %.2f \t %c \t %.2f", id[r], grades[r][1], grades[r][2], grades[r][3], grades[r][4], ch, avg[r]);
+}
+
+
+int main(void)
+{
+	FILE * file = fopen("ielts.txt", "r");
+	double grades[MAX][SIZE];
+	int id[MAX];
+	int num;
+	double sum[MAX];
+	double avg[MAX];
+	char ch[MAX];
+	if (file == NULL)
+		printf("Cannot open");
+	else
+	{
+		int r, c;
+
+
+		num = readFromFile(grades, id, file);
+		for (r = 0; r < num; r++)
+		{
+			for (c = 1; c < 5; c++)
+			{
+				sum[r] += grades[r][c];
+			}
+			avg[r] = sum[r] / 4;
+			if (avg[r] > 6.5)
+				ch[r] = 'Y';
+			else 
+				ch[r] = 'N';
+		}
+
+		display(grades, id, num, ch, avg);
+			
+		fclose(file);
 	}
-	return total;
+	return(0);
 }
 
-int subtraction(int num1, int num2) {
-	while (num2 >= num1) {
-		num2 -= num1;
-	}
-	return num2;
-}
 
-int main(void) {
-	int num1, num2,total;	
 
-	do {
-		printf("Enter the num1 and num2: ");
-		scanf(" %d %d", &num1, &num2);
-		
-		if (num1 > num2) {
-			total=summation(num1, num2);
-			printf("%d > %d => Result of Operation: %d\n", num1, num2, total);
-		}
-		else if (num1 < num2) {
-			total=subtraction(num1, num2);
-			printf("%d < %d => Result of Operation: %d\n", num1, num2, total);
-		}
-		else {
-			printf("Goodbyee :D");
-			break;
-		}
-
-	} while (num1 != num2);
-
-	return 0;
-}
